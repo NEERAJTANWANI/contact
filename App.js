0790bad38d85d2554166d77875ebff6ed821a66f
unknown <neeraj.tanwani28@gmail.com>
2020-07-03 16:59:12 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+import HomeScreen from './screens/HomeScreen';
+import AddNewContactScreen from './screens/AddNewContactScreen';
+import EditContactScreen from './screens/EditContactScreen';
+import ViewContactScreen from './screens/ViewContactScreen';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack =createStackNavigator();
+function App(){
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Contact App"
+          component={HomeScreen}
+          options={{ 
+            headerTintColor: "#fff",
+            headerStyle: {
+              backgroundColor: "#f5412e",
+            },
+            headerTitleStyle:{
+              fontWeight:'bold',
+              color:"#fff",
+            },
+            }}/>
+        <Stack.Screen
+          name="Add New Contact"
+          component={AddNewContactScreen}/>
+        <Stack.Screen
+          name="Edit Contact"
+          component={EditContactScreen}/>
+        <Stack.Screen
+          name="View Contact"
+          component={ViewContactScreen}/>
+          
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
